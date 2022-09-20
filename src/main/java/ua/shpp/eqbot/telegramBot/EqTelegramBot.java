a4d5c48519908610b1337f9a6f33534e898d7c82
IvanKovalov <kovalevvana8@gmail.com>
2022-09-20 20:29:10 +0300

    In this commit changed connection to telegramApi
    deleted realization of echo

--- src/main/java/ua/shpp/eqbot/telegramBot/EqTelegramBot.java
+++ src/main/java/ua/shpp/eqbot/telegramBot/EqTelegramBot.java
@@ -3,10 +3,7 @@ package ua.shpp.eqbot.telegramBot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class EqTelegramBot extends TelegramLongPollingBot {
@@ -18,20 +15,7 @@ public class EqTelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage()) {
-            Message message = update.getMessage();
-            if (message.hasText()) {
-                String text = message.getText();
-                SendMessage sm = new SendMessage();
-                sm.setText("Ви відправили: " + text);
-                sm.setChatId(String.valueOf(message.getChatId()));
-                try {
-                    execute(sm);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+
     }
 
     @Override
