deccdf8ba5a379d23880928b07867adbe881494c
Michael Tsurkan <tsurkanmixail@gmail.com>
2022-09-21 00:00:36 +0300

    TBQ-24: * edit ping/pong backend

--- src/main/java/ua/shpp/eqbot/telegrambot/EqTelegramBot.java
+++ src/main/java/ua/shpp/eqbot/telegrambot/EqTelegramBot.java
@@ -3,10 +3,13 @@ package ua.shpp.eqbot.telegrambot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class EqTelegramBot extends TelegramLongPollingBot {
+    // TODO: 20.09.2022
     @Value("${telegram.bot.name}")
     private String botUsername;
 
@@ -15,7 +18,21 @@ public class EqTelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            long chatId = update.getMessage().getChatId();
+            sendingResponse(chatId, update.getMessage().getText() );
+        }
+    }
 
+    private void sendingResponse(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try {
+            execute(message);
+        }catch(TelegramApiException ex){
+            // TODO: 20.09.2022 add logger
+        }
     }
 
     @Override
