e112dadbc3f12607ec1143740aa38705572f54a7
Vitaliy <kornilchenko@gmail.com>
2022-09-20 23:53:34 +0300

    TBQ-13: * Change class bot

--- src/main/java/ua/shpp/eqbot/telegrambot/EqTelegramBot.java
+++ src/main/java/ua/shpp/eqbot/telegrambot/EqTelegramBot.java
@@ -3,7 +3,9 @@ package ua.shpp.eqbot.telegrambot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class EqTelegramBot extends TelegramLongPollingBot {
@@ -15,7 +17,21 @@ public class EqTelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            long chatId = update.getMessage().getChatId();
+            sendingResponse(chatId, update.getMessage().getText() );
+        }
+    }
 
+    private void sendingResponse(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try {
+            execute(message);
+        }catch(TelegramApiException ex){
+           // log.error("message sending error");
+        }
     }
 
     @Override
