475b2192d649e007088192051da96d889ff4e8a9
Vitaliy <kornilchenko@gmail.com>
2022-09-20 15:52:50 +0300

    Create BotConfig

---
+++ src/main/java/ua/shpp/eqbot/config/BotConfig.java
@@ -0,0 +1,36 @@
+package ua.shpp.eqbot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.properties")
+public class BotConfig {
+
+    //check properties file parameters name and token
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.token}")
+    String token;
+
+    public BotConfig() {
+    }
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setBotName(String botName) {
+        this.botName = botName;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
