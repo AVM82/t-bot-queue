81cf531ee02e562a0b158f7b0024a3ff28d6c25c
IvanKovalov <kovalevvana8@gmail.com>
2022-09-20 19:32:25 +0300

    In this commit added telegramApi
    Connected to pom.xml 2 dependencies for getting access to our telegramBot Api. Added bean-configuration EqTelegrambot in telegrmBot folder. In this bean we install bot`s name and token for our values. Redefined onUpdateReceived method: we received text from our user, and send to him in telegram chat answer with such template "Ви відправили + User.text"

---
+++ src/main/java/ua/shpp/eqbot/telegramBot/EqTelegramBot.java
@@ -0,0 +1,46 @@
+package ua.shpp.eqbot.telegramBot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class EqTelegramBot extends TelegramLongPollingBot {
+    @Value("${telegram.bot.name}")
+    private String botUsername;
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                String text = message.getText();
+                SendMessage sm = new SendMessage();
+                sm.setText("Ви відправили: " + text);
+                sm.setChatId(String.valueOf(message.getChatId()));
+                try {
+                    execute(sm);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
