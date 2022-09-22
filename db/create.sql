f599984650a36ccec7e59cfa52b43d01482f3279
Sekator778@ <sekator778@gmail.com>
2022-09-22 21:34:23 +0200

    create scheme db

--- db/create.sql
+++ db/create.sql
@@ -1,52 +1,24 @@
-drop table if exists type_service cascade;
-drop table if exists rank cascade;
-drop table if exists service_provider cascade;
-drop table if exists service cascade;
-drop table if exists provider cascade;
-
-create table if not exists type_service
+create table if not exists provider
 (
-    id_type     serial primary key,
-    name        varchar(200) not null,
-    description varchar(2000)
-);
-
-create table if not exists rank
-(
-    id_rank serial primary key,
-    name    varchar(2000) not null
+    id                serial primary key,
+    name              varchar(200) not null,
+    address           varchar(200) not null,
+    date_registration timestamp    NOT NULL DEFAULT NOW(),
+    description       text
 );
 
 create table if not exists service
 (
-    id_service     serial primary key,
-    name           varchar(2000) not null,
-    type_id        INT,
-    cost           decimal,
-    CONSTRAINT FK_service_type FOREIGN KEY (type_id)
-        REFERENCES type_service (id_type),
-    time_available timestamp     not null
+    id          serial primary key,
+    name        varchar(80) not null,
+    description text,
+    provider_id int,
+    CONSTRAINT fk_provider
+        FOREIGN KEY (provider_id)
+            REFERENCES provider (id)
 );
 
-create table if not exists provider
-(
-    provider_id       serial primary key,
-    name              varchar(200) not null,
-    address           varchar(200) not null,
-    date_registration timestamp    NOT NULL DEFAULT NOW(),
-    service_id        int,
-    rank              int,
-    CONSTRAINT FK_provider_rank FOREIGN KEY (rank)
-        REFERENCES rank (id_rank),
-    description       varchar(2000)
-);
 
-create table if not exists service_provider
-(
-    service_id  int references service (id_service) ON UPDATE CASCADE ON DELETE CASCADE,
-    provider_id int references provider (provider_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT service_provider_pkey PRIMARY KEY (service_id, provider_id) -- explicit pk
-);
 
 
 
