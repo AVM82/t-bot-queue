d480c129a49e67c63812dae803e1ed9c4845a49b
Vitaliy <kornilchenko@gmail.com>
2022-09-24 23:58:40 +0300

    TBQ-26: * add logger

--- src/main/java/ua/shpp/eqbot/messagesender/MessageSenderImpl.java
+++ src/main/java/ua/shpp/eqbot/messagesender/MessageSenderImpl.java
@@ -1,5 +1,7 @@
 package ua.shpp.eqbot.messagesender;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -8,6 +10,7 @@ import ua.shpp.eqbot.telegrambot.EqTelegramBot;
 
 @Component
 public class MessageSenderImpl implements MessageSender{
+    Logger log = LoggerFactory.getLogger(MessageSenderImpl.class);
     private EqTelegramBot bot;
 
     @Override
@@ -15,7 +18,7 @@ public class MessageSenderImpl implements MessageSender{
         try {
             bot.execute(message);
         }catch (TelegramApiException e) {
-            //TODO logger
+            log.warn(e.getMessage());
         }
     }
 
