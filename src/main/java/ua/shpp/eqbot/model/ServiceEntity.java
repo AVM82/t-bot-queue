28870a22506d204c20a33b0c315366b7e5dd56d6
Vitaliy <kornilchenko@gmail.com>
2022-09-28 19:23:29 +0300

    TBQ-9: * added a fields time work

--- src/main/java/ua/shpp/eqbot/model/ServiceEntity.java
+++ src/main/java/ua/shpp/eqbot/model/ServiceEntity.java
@@ -13,6 +13,157 @@ public class ServiceEntity {
     private Long id_telegram;
     private String name;
     private String description;
+    private String startWorkInSunday;
+    private String endWorkInSunday;
+    private String startWorkInMonday;
+    private String endWorkInMonday;
+    private String startWorkInTuesday;
+    private String endWorkInTuesday;
+    private String startWorkInWednesday;
+    private String endWorkInwWednesday;
+    private String startWorkInThursday;
+    private String endWorkInThursday;
+    private String startWorkInFriday;
+    private String endWorkInFriday;
+    private String startWorkInSaturday;
+    private String endWorkInSaturday;
+
+    public String getStartWorkInSunday() {
+        return startWorkInSunday;
+    }
+
+    public ServiceEntity setStartWorkInSunday(String startWorkInSunday) {
+        this.startWorkInSunday = startWorkInSunday;
+        return this;
+    }
+
+    public String getEndWorkInSunday() {
+        return endWorkInSunday;
+    }
+
+    public ServiceEntity setEndWorkInSunday(String endWorkInSunday) {
+        this.endWorkInSunday = endWorkInSunday;
+        return this;
+    }
+
+    public String getStartWorkInMonday() {
+        return startWorkInMonday;
+    }
+
+    public ServiceEntity setStartWorkInMonday(String startWorkInMonday) {
+        this.startWorkInMonday = startWorkInMonday;
+        return this;
+    }
+
+    public String getEndWorkInMonday() {
+        return endWorkInMonday;
+    }
+
+    public ServiceEntity setEndWorkInMonday(String endWorkInMonday) {
+        this.endWorkInMonday = endWorkInMonday;
+        return this;
+    }
+
+    public String getStartWorkInTuesday() {
+        return startWorkInTuesday;
+    }
+
+    public ServiceEntity setStartWorkInTuesday(String startWorkInTuesday) {
+        this.startWorkInTuesday = startWorkInTuesday;
+        return this;
+    }
+
+    public String getEndWorkInTuesday() {
+        return endWorkInTuesday;
+    }
+
+    public ServiceEntity setEndWorkInTuesday(String endWorkInTuesday) {
+        this.endWorkInTuesday = endWorkInTuesday;
+        return this;
+    }
+
+    public String getStartWorkInWednesday() {
+        return startWorkInWednesday;
+    }
+
+    public ServiceEntity setStartWorkInWednesday(String startWorkInWednesday) {
+        this.startWorkInWednesday = startWorkInWednesday;
+        return this;
+    }
+
+    public String getEndWorkInwWednesday() {
+        return endWorkInwWednesday;
+    }
+
+    public ServiceEntity setEndWorkInwWednesday(String endWorkInwWednesday) {
+        this.endWorkInwWednesday = endWorkInwWednesday;
+        return this;
+    }
+
+    public String getStartWorkInThursday() {
+        return startWorkInThursday;
+    }
+
+    public ServiceEntity setStartWorkInThursday(String startWorkInThursday) {
+        this.startWorkInThursday = startWorkInThursday;
+        return this;
+    }
+
+    public String getEndWorkInThursday() {
+        return endWorkInThursday;
+    }
+
+    public ServiceEntity setEndWorkInThursday(String endWorkInThursday) {
+        this.endWorkInThursday = endWorkInThursday;
+        return this;
+    }
+
+    public String getStartWorkInFriday() {
+        return startWorkInFriday;
+    }
+
+    public ServiceEntity setStartWorkInFriday(String startWorkInFriday) {
+        this.startWorkInFriday = startWorkInFriday;
+        return this;
+    }
+
+    public String getEndWorkInFriday() {
+        return endWorkInFriday;
+    }
+
+    public ServiceEntity setEndWorkInFriday(String endWorkInFriday) {
+        this.endWorkInFriday = endWorkInFriday;
+        return this;
+    }
+
+    public String getStartWorkInSaturday() {
+        return startWorkInSaturday;
+    }
+
+    public ServiceEntity setStartWorkInSaturday(String startWorkInSaturday) {
+        this.startWorkInSaturday = startWorkInSaturday;
+        return this;
+    }
+
+    public String getEndWorkInSaturday() {
+        return endWorkInSaturday;
+    }
+
+    public ServiceEntity setEndWorkInSaturday(String endWorkInSaturday) {
+        this.endWorkInSaturday = endWorkInSaturday;
+        return this;
+    }
+
+    public String getTimeBetweenClients() {
+        return timeBetweenClients;
+    }
+
+    public ServiceEntity setTimeBetweenClients(String timeBetweenClients) {
+        this.timeBetweenClients = timeBetweenClients;
+        return this;
+    }
+
+    private String timeBetweenClients;
 
     public ServiceEntity() {
     }
