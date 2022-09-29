dc56cfcef706250e7c1d9073720fe381a8cce87d
Сергій Коженівський <outnumbered34@gmail.com>
2022-09-29 19:21:30 +0300

    Tbq12: Adding internationalization and do not allow to create 2 services with the same name

--- src/main/java/ua/shpp/eqbot/repository/ServiceRepository.java
+++ src/main/java/ua/shpp/eqbot/repository/ServiceRepository.java
@@ -1,7 +1,13 @@
 package ua.shpp.eqbot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ua.shpp.eqbot.model.ServiceEntity;
 
 public interface ServiceRepository extends JpaRepository<ServiceEntity, Long> {
+
+    ServiceEntity getFirstByName(String name);
+
+    @Query("select t from ServiceEntity t where t.name = ?1 and t.id_telegram=?2")
+    ServiceEntity getFirstByNameAndAndId_telegram(String name, Long id_telegram);
 }
