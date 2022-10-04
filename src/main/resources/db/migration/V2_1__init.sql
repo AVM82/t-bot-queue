0dca3da335d6f45dcf904838e310258ca239a917
Sekator778@ <sekator778@gmail.com>
2022-10-04 10:27:54 +0200

    changes in the operation of the flyway, as the columns were changed, and now the situation is not clear

--- src/main/resources/db/migration/V2_1__init.sql
+++ src/main/resources/db/migration/V2_1__init.sql
@@ -1,20 +1,6 @@
-alter table appuser
-    rename column id_telegram to "telegramId";
+create unique index appuser_telegram_id_uindex
+    on appuser (telegram_id);
 
-create unique index appuser_telegramid_uindex
-    on appuser ("telegramId");
-
-alter table appuser
-    rename column time_created to "createdTime";
-
-alter table provider_entity
-    rename column city to "providerCity";
-
-alter table provider_entity
-    rename column id_telegram to "telegramId";
-
-alter table services
-    rename column id_telegram to "telegramId";
 
 
 
