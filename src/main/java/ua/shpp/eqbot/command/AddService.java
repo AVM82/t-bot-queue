e51d183e72664f058573e5ee5fee3da2c6684b18
Vitaliy <kornilchenko@gmail.com>
2022-10-04 22:23:51 +0300

    TBQ-9 * change format time registration

--- src/main/java/ua/shpp/eqbot/command/AddService.java
+++ src/main/java/ua/shpp/eqbot/command/AddService.java
@@ -170,7 +170,7 @@ public class AddService implements Command {
     }
 
     private boolean changeFormatTime(String time, Long id){
-        if (time.matches("([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]"))
+        if (time.matches("([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]-([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]"))
             return true;
         else {
             createMessage(id,"unformatted");
