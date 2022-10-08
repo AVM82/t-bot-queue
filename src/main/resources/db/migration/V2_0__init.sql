70c6b77454bf668606ae0a495a7871393590d22c
Sekator778@ <sekator778@gmail.com>
2022-10-08 13:02:28 +0200

    add fields  to services tables

--- src/main/resources/db/migration/V2_0__init.sql
+++ src/main/resources/db/migration/V2_0__init.sql
@@ -18,13 +18,20 @@ create table if not exists provider_entity
     provider_city varchar(255),
     telegram_id   bigint
 );
-create table if not exists services
+create table services
 (
-    id          bigserial
+    id                      bigserial
         primary key,
-    avatar      bytea,
-    description varchar(255),
-    name        varchar(255),
-    telegram_id bigint
+    avatar                  bytea,
+    description             varchar(255),
+    friday_working_hours    varchar(255),
+    monday_working_hours    varchar(255),
+    name                    varchar(255),
+    saturday_working_hours  varchar(255),
+    sunday_working_hours    varchar(255),
+    telegram_id             bigint,
+    thursday_working_hours  varchar(255),
+    time_between_clients    varchar(255),
+    tuesday_working_hours   varchar(255),
+    wednesday_working_hours varchar(255)
 );
-
