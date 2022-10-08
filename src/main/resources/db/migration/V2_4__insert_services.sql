76c43a9f8cbfd8c54dd7108be9832ad0f19c9e50
Sekator778@ <sekator778@gmail.com>
2022-10-08 12:32:58 +0200

    I wanted to add a service, but there were a lot of fields, so I didn't finish it

---
+++ src/main/resources/db/migration/V2_4__insert_services.sql
@@ -0,0 +1,6 @@
+truncate table services restart identity;
+insert into services(description, name, telegram_id)
+values ('Виробництво мягких іграшок', 'Мягкі іграшки', 123),
+       ('Very taste hamburgers', 'Food', '456'),
+       ('Ремонт взуття', 'Черевик', '789'),
+       ('Стрижка волосся', 'Перукар', '987');
