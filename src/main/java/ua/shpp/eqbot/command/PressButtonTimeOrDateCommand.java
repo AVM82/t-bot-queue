371b7df2d530b3ffaef66779be2f6cb4051b2aef
Vitaliy <kornilchenko@gmail.com>
2022-10-11 20:24:55 +0300

    + add new position menu and class PressButtonTimeOrDateCommand

---
+++ src/main/java/ua/shpp/eqbot/command/PressButtonTimeOrDateCommand.java
@@ -0,0 +1,11 @@
+package ua.shpp.eqbot.command;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class PressButtonTimeOrDateCommand implements ICommand {
+
+    @Override
+    public boolean execute(Update update) {
+        return false;
+    }
+}
