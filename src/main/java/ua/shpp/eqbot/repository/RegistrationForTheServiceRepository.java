05e5ed6a644cbb37ece71674a8bfeb94d6e54374
Michael Tsurkan <tsurkanmixail@gmail.com>
2022-10-14 14:24:23 +0300

    add scheduled

--- src/main/java/ua/shpp/eqbot/repository/RegistrationForTheServiceRepository.java
+++ src/main/java/ua/shpp/eqbot/repository/RegistrationForTheServiceRepository.java
@@ -1,12 +1,16 @@
 package ua.shpp.eqbot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ua.shpp.eqbot.model.RegistrationForTheServiceEntity;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
 
 @Repository
 public interface RegistrationForTheServiceRepository extends JpaRepository<RegistrationForTheServiceEntity, Long> {
-    //List<RegistrationForTheServiceEntity> findAllBy(Long id);
+    @Query("SELECT e FROM RegistrationForTheServiceEntity e WHERE e.serviceRegistrationDateTime >= ?1 AND e.serviceRegistrationDateTime < ?2")
+    List<RegistrationForTheServiceEntity> findAllBetweenDates(LocalDateTime from, LocalDateTime until);
 }
