12f487bb7087ab1c1c2d155b3d5fd97db3ee7ae4
IvanKovalov <kovalevvana8@gmail.com>
2022-10-15 21:05:17 +0300

    * changed logic of internationalization

--- src/main/java/ua/shpp/eqbot/command/SendNotificationToProviderCommand.java
+++ src/main/java/ua/shpp/eqbot/command/SendNotificationToProviderCommand.java
@@ -2,6 +2,7 @@ package ua.shpp.eqbot.command;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import ua.shpp.eqbot.internationalization.BundleLanguage;
 import ua.shpp.eqbot.model.ServiceEntity;
 import ua.shpp.eqbot.repository.ServiceRepository;
 import ua.shpp.eqbot.service.SendBotMessageService;
@@ -9,22 +10,22 @@ import ua.shpp.eqbot.service.SendBotMessageService;
 public class SendNotificationToProviderCommand implements ICommand {
     private final ServiceRepository serviceRepository;
     private final SendBotMessageService sendBotMessageService;
+    private final BundleLanguage bundleLanguage;
 
-    public SendNotificationToProviderCommand(ServiceRepository serviceRepository, SendBotMessageService sendBotMessageService) {
+    public SendNotificationToProviderCommand(ServiceRepository serviceRepository, SendBotMessageService sendBotMessageService, BundleLanguage bundleLanguage) {
         this.serviceRepository = serviceRepository;
         this.sendBotMessageService = sendBotMessageService;
+        this.bundleLanguage = bundleLanguage;
     }
 
-    public void sendNotification(Long serviceId, String timeOfRegistration, String userName, String language) {
+    public void sendNotification(Long serviceId, String timeOfRegistration, String userName) {
         ServiceEntity serviceEntity = serviceRepository.findFirstById(serviceId);
         Long providerChatId = serviceEntity.getTelegramId();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(providerChatId);
-        if (language.equals("en")) {
-            sendMessage.setText("New customer " + userName + " registered to your service: " + serviceEntity.getName() + " at time: " + timeOfRegistration);
-        } else {
-            sendMessage.setText("Новий клієнт " + userName + " заруєструвався до вашого сервісу: " + serviceEntity.getName() + " на дату: " + timeOfRegistration);
-        }
+        String internationalizationAnswer = bundleLanguage.getValue(providerChatId, "send_notification_to_provider");
+        String [] answer = internationalizationAnswer.split("\\.");
+        sendMessage.setText(answer[0] + userName + answer[1] + serviceEntity.getName() + answer[2] + timeOfRegistration);
         sendBotMessageService.sendMessage(sendMessage);
     }
 
