53169a2bc4bc5e253c0e46e8b45c88bf1aef655d
Vitaliy <kornilchenko@gmail.com>
2022-10-16 11:55:59 +0300

    *change registration for the service in RegistrationForTheServiceCommand

--- src/main/java/ua/shpp/eqbot/command/registrationfortheservice/RegistrationForTheServiceCommand.java
+++ src/main/java/ua/shpp/eqbot/command/registrationfortheservice/RegistrationForTheServiceCommand.java
@@ -1,6 +1,5 @@
 package ua.shpp.eqbot.command.registrationfortheservice;
 
-import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
